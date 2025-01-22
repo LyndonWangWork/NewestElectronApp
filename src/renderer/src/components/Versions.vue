f326945af0281b2fba7a2641d9757b237638d313
wangyiping <lyndon.wang.work@gmail.com>
2025-01-22 23:34:43 +0800

    feat(renderer): 重新设计版本信息组件并添加新功能
    - 重新设计了版本信息组件的布局和样式，使其更加美观和响应式
    - 添加了打开文档和发送IPC事件的按钮，增强了用户交互
    - 在样式文件中添加了新的tab背景颜色变量

--- src/renderer/src/components/Versions.vue
+++ src/renderer/src/components/Versions.vue
@@ -2,12 +2,37 @@
 import { reactive } from 'vue'
 
 const versions = reactive({ ...window.electron.process.versions })
+
+const ipcHandle = () => window.electron.ipcRenderer.send('ping')
 </script>
 
 <template>
-  <ul class="versions">
-    <li class="electron-version">Electron v{{ versions.electron }}</li>
-    <li class="chrome-version">Chromium v{{ versions.chrome }}</li>
-    <li class="node-version">Node v{{ versions.node }}</li>
-  </ul>
+  <div class="versions flex text-sm">
+    <div class="flex">
+      <div>
+        <a
+          href="https://electron-vite.org/"
+          target="_blank"
+          rel="noreferrer"
+          class="mr-1 bg-slate-500 px-2 py-1 rounded-sm cursor-pointer"
+          >Doc</a
+        >
+        <a
+          class="bg-slate-500 px-2 py-1 rounded-sm cursor-pointer"
+          target="_blank"
+          rel="noreferrer"
+          @click="ipcHandle"
+          >IPC evt</a
+        >
+      </div>
+      <div>
+        <p class="ml-1">Press<code>F12</code>to open devTool</p>
+      </div>
+    </div>
+    <ul class="flex items-center">
+      <li class="electron-version">Electron v{{ versions.electron }}</li>
+      <li class="chrome-version">Chromium v{{ versions.chrome }}</li>
+      <li class="node-version">Node v{{ versions.node }}</li>
+    </ul>
+  </div>
 </template>
