68944e4038d58681b8ae3086dba898e1b75d2afe
wangyiping <lyndon.wang.work@gmail.com>
2025-02-04 00:44:17 +0800

    ✨ feat: 合并master修改

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,6 +1,28 @@
-import './assets/main.css'
+import type { UserModule } from './types'
 
-import { createApp } from 'vue'
+import { setupLayouts } from 'virtual:generated-layouts'
+import { ViteSSG } from 'vite-ssg'
+import { routes } from 'vue-router/auto-routes'
 import App from './App.vue'
+import { createWebHashHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import '@unocss/reset/tailwind.css'
+import './styles/main.css'
+import 'uno.css'
+
+// https://github.com/antfu/vite-ssg
+export const createApp = ViteSSG(
+  App,
+  {
+    routes: setupLayouts(routes),
+    base: import.meta.env.BASE_URL,
+    history: createWebHashHistory()
+  },
+  (ctx) => {
+    // install all modules under `modules/`
+    Object.values(
+      import.meta.glob<{ install: UserModule }>('./modules/*.ts', { eager: true })
+    ).forEach((i) => i.install?.(ctx))
+    // ctx.app.use(Previewer)
+  }
+)
