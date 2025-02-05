5725394ce97597289176b91a7f9fa31afd4e3379
wangyiping <lyndon.wang.work@gmail.com>
2025-02-05 11:41:12 +0800

    ✨ feat: merge master

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,80 +1,29 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import Versions from './components/Versions.vue'
-import Tabs from '@renderer/components/Tabs.vue'
-// import Header from '@renderer/components/Header.vue'
-import { MENU_BAR_HEIGHT } from '@/common/const'
-
-import RendererBus from '@/common/PortBus/renderer'
-// RendererBus.init()
-
-const selectionText = ref('')
-const pos = ref([0, 0])
-RendererBus.on('selectionText', (messageEvent, ...args) => {
-  const [data] = args
-  selectionText.value = data.text
-  pos.value = data.pos
+// https://github.com/vueuse/head
+// you can use this to manipulate the document head in any components,
+// they will be rendered correctly in the html results with vite-ssg
+useHead({
+  title: 'Vitesse',
+  meta: [
+    {
+      name: 'description',
+      content: 'Opinionated Vite Starter Template',
+    },
+    {
+      name: 'theme-color',
+      content: () => isDark.value ? '#00aba9' : '#ffffff',
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: () => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg',
+    },
+  ],
 })
-
-// RendererBus.on('tabChanged', (_messageEvent, ...args) => {
-//   const [allTabs] = args
-//   console.log(allTabs.tabs)
-//   // allTabs.tabs
-//   //   .filter((t: ElectronTab) => !tabs.value.find((_t) => _t.key === t.key))
-//   //   .forEach((t: ElectronTab) => {
-//   //     tabRef.value?.addTab(t)
-//   //   })
-// })
-
-// const tabs = ref<ElectronTab[]>([
-//   {
-//     label: 'Home',
-//     closable: false,
-//     key: 'Home'
-//   }
-// ])
-const tabRef = ref<InstanceType<typeof Tabs>>()
-const handleClick = (label: string, url: string) => {
-  // console.log('handleClick', label, url)
-  RendererBus.emit('addTab', { event: 'addTab', label, url })
-}
-
-// window.onmessage = (event) => {
-//   console.log('onmessage', event)
-//   // event.source === window 意味着消息来自预加载脚本
-//   // 而不是来自iframe或其他来源
-//   if (event.source === window && event.data === 'register-port') {
-//     const [port] = event.ports
-//     // 一旦我们有了这个端口，我们就可以直接与主进程通信
-//     port.onmessage = (event) => {
-//       console.log('from main process:', event.data)
-//       port.postMessage(event.data.test * 2)
-//     }
-//   }
-// }
 </script>
 
 <template>
-  <div
-    class="absolute flex top-0 left-0 w-full bg-(--tab-background-color)"
-    :style="{ height: MENU_BAR_HEIGHT + 'px' }"
-  >
-    <!-- <Header title="Hello Electron"></Header> -->
-    <Tabs ref="tabRef"></Tabs>
-  </div>
-  <div class="text">
-    <div class="p-2 cursor-pointer" @click="handleClick('baidu', 'https://www.baidu.com')">
-      Baidu
-    </div>
-    <div class="p-2 cursor-pointer" @click="handleClick('bing', 'https://www.bing.com')">Bing</div>
-    <div class="p-2">Selection Text: {{ selectionText }}</div>
-    <div class="p-2">pos: {{ pos }}</div>
-  </div>
-
-  <Versions />
+  <RouterView />
 </template>
-<style lang="less" scoped>
-.custom-titlebar {
-  -webkit-app-region: drag;
-}
-</style>
